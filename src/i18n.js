fb5090ff4886a522d96103761fed8e17b93e41d4
Анна Рачкова <annarachkova@MacBook-Air-Anna.local>
2025-04-20 23:48:59 +0600

    fix2

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,26 @@
+// i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 
-i18n
+const i18nInitPromise = i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "ru",
     debug: false,
-    interpolation: { escapeValue: false },
+    interpolation: {
+      escapeValue: false,
+    },
     backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
+      loadPath: "public/locales/{{lng}}/translation.json",
+    },
+    react: {
+      useSuspense: false,
     },
   });
 
+export { i18nInitPromise };
 export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./i18n";
 import { BrowserRouter } from "react-router-dom";
-import '@ant-design/v5-patch-for-react-19';
+import "@ant-design/v5-patch-for-react-19";
+import "./i18n";
+import { i18nInitPromise } from "./i18n"; // <-- импортируешь промис
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+i18nInitPromise.then(() => {
+  createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+});
